97ab669936bbc22bb5453903993910c8658d2d9a
elicookies <elikoleva32@gmail.com>
2023-02-08 21:54:19 +0000

    added a navigation bar using bootstrap and some jquery

--- LabsSDEV/WEEK2/app/routes.py
+++ LabsSDEV/WEEK2/app/routes.py
@@ -6,12 +6,6 @@ from flask import request
 @app.route('/')
 @app.route('/index')
 def index():
-    user ={'username': request.form.get('username')}
+    user ={'username':'Elitsa'}
     # return "Welcome to Lab Class WEEK TWO -Let the fun begin!"
-    return render_template('index.html', title='Week 2', user=user)
-
-
-@app.route('/data_input/', methods=('GET', 'POST'))
-def data_input():
-    user = {'username': request.form.get('username')}
     return render_template('index.html', title='Week 2', user=user)
