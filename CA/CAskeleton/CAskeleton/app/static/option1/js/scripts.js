2c455fc6e97b97130aca84bdf321c16dda072acf
elicookies <elikoleva32@gmail.com>
2023-04-23 03:04:30 +0100

    finland map clickable

--- CA/CAskeleton/CAskeleton/app/static/option1/js/scripts.js
+++ CA/CAskeleton/CAskeleton/app/static/option1/js/scripts.js
@@ -5,3 +5,35 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function toggleChildren() {
+  var checkBox = document.getElementById("child");
+  var childAge = document.getElementById("child_age");
+  if (checkBox.checked) {
+    childAge.style.display = "block";
+  } else {
+    childAge.style.display = "none";
+  }
+}
+
+  // Get all the prefecture elements
+  const prefectures = document.querySelectorAll('.prefecture');
+
+  // Add event listeners to each prefecture
+  prefectures.forEach(prefecture => {
+    prefecture.addEventListener('click', (event) => {
+      // Remove the 'selected' class from any previously selected element
+      const previouslySelected = document.querySelector('.selected');
+      if (previouslySelected) {
+        previouslySelected.classList.remove('selected');
+      }
+
+      // Add the 'selected' class to the clicked element
+      event.currentTarget.classList.add('selected');
+
+      // Store the selected value, e.g., in a hidden input field
+      const selectedValue = event.currentTarget.id;
+      // Do something with the selected value, e.g., update a form field
+      console.log('Selected prefecture:', selectedValue);
+    });
+  });
