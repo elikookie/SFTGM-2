f1fede16ac5302dc9ddab97c1ea962b9fed16db5
elicookies <elikoleva32@gmail.com>
2023-04-29 21:49:10 +0100

    options reflect lang

--- CA/CAskeleton/CAskeleton/app/__init__.py
+++ CA/CAskeleton/CAskeleton/app/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #If we are using session then we need to set a secret key
 app.secret_key='a secret key'
 def get_locale():
-    return 'fr'
+    return 'fi'
 
 #Hook Babel into your app and pass the local returned by get_locale to it
 babel = Babel(app, locale_selector=get_locale)
--- CA/CAskeleton/CAskeleton/app/routes.py
+++ CA/CAskeleton/CAskeleton/app/routes.py
@@ -1,20 +1,28 @@
 from app import app
 from flask import render_template, session, redirect, request, url_for
 
-
 @app.route('/', methods=['GET', 'POST'])
 def start():
     if request.method == 'POST':
         session['option'] = request.form['option']  # set session variable based on user selection
-#       return 'Option selected: {}'.format(session['option'])# you can comment this out if using this approach in your CA
-        return(redirect(url_for("index")))
+        if session['option'] == 'japanese':
+            return redirect(url_for('japanese_home'))
+        elif session['option'] == 'finnish':
+            return redirect(url_for('finnish_home'))
     return render_template('setoption.html')
 
-@app.route('/index')
-def index():
-    return (render_template(session['option'] + "/index.html", opt=session['option']))
+@app.route('/japanese_home')
+def japanese_home():
+    session['japanese'] = 'japanese'
+    return (render_template("japanese/japanese_home.html"))
+
+
+@app.route('/finnish_home')
+def finnish_home():
+    session['finnish'] = 'finnish'
+    return (render_template("finnish/finnish_home.html"))
 
 
 @app.route('/about')
 def about():
-    return (render_template ("about.html"))
+    return (render_template("about.html"))
